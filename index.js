e48080a1eb2c13c16507938f39c4d3585af8e74e
andreselianor <113841548+andreselianor@users.noreply.github.com>
2025-12-01 18:26:35 +0100

    feat: added list with result use-case

---
+++ index.js
@@ -0,0 +1,15 @@
+let sheeps = 0;
+let wolfs = 0;
+
+/*/ OPCIONES POSIBLES //
+
+1._ La lista contiene alguna oveja:
+    - 'There are {numero} sheep in total'
+
+2._ La lista no tiene ovejas, pero SÍ lobos:
+    - 'UPS!!! Wolfs eaten sheeps'
+
+3._ La lista no contiene ovejas NI lobos:
+    - 'The list is empty'
+
+*/
